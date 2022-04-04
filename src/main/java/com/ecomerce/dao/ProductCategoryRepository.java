eb1e49a311b8f3a11ad164fa4304932175fa950f
farhat <hichem.farhat95@gmail.com>
2022-04-04 12:17:35 +0100

    back

---
+++ src/main/java/com/ecomerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.ecomerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import com.ecomerce.model.ProductCategory;
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository <ProductCategory, Long>{
+
+}
